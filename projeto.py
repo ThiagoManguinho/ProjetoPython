39c3a05af5d90a914f6c9f509ac599a046bf64b0
Tonho <>
2023-11-10 20:15:04 -0300

    excluir melhorado
    melhorei o sistema de excluir

--- projeto.py
+++ projeto.py
@@ -1,6 +1,9 @@
-def excluir() :
-    livro = input("Qual o nome do livro").lower()
-    lista = {}
-    for livro in lista :
-        lista = lista.pop(livro)
+def execluir():
+    n = int(input("Quantos livros voce quer deletar :"))
+    for i in range(n):
+        livro = input("Qual o nome do livro :").lower()
+        if lista.get(livro):
+            lista.pop(livro)
+        else :
+            print("Esse livro não esta na lista")
     return lista 
