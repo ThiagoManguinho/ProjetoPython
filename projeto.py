ea4a94a2000e064947bb348c3c99d59f4a1ce1de
Tonho <>
2023-11-10 21:08:49 -0300

    Excluido melhor
    melhorei mais

--- projeto.py
+++ projeto.py
@@ -1,9 +1,17 @@
 def execluir():
+#Escolhendo o numero de livros a serem deletados
     n = int(input("Quantos livros voce quer deletar :"))
     for i in range(n):
+
+        #Escolhendo o livro 
         livro = input("Qual o nome do livro :").lower()
+
+        #Deletando o dicionario
         if lista.get(livro):
             lista.pop(livro)
+            if lista == {} :
+                print("Sua lista esta vazia")
         else :
             print("Esse livro não esta na lista")
+        #Retornando a lista nova 
     return lista 
