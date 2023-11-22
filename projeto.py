01a72e7c73928da4dbca3e323208505e3ef257fb
Tonho <joaoamlp@gmail.com>
2023-11-22 09:28:34 -0300

    Excluir definitivo

--- projeto.py
+++ projeto.py
@@ -1,17 +1,30 @@
-def execluir():
+def excluir():
+    f = open(".txt", "+r")
+    lista = f.readline()
 #Escolhendo o numero de livros a serem deletados
-    n = int(input("Quantos livros voce quer deletar :"))
-    for i in range(n):
-
+    print(f.read())
+    print("-"*110)
+    print("Ao escrever sair o programa encerrara".capitalize())
+    while True:
+        
         #Escolhendo o livro 
-        livro = input("Qual o nome do livro :").lower()
+        livro = input("Qual o nome do livro :").upper()
+
+
 
         #Deletando o dicionario
-        if lista.get(livro):
-            lista.pop(livro)
-            if lista == {} :
+        try :
+            if lista.get(livro):
+                lista.pop(livro)
+                print("Livros excluidos com sucesso")
+            if lista == () :
                 print("Sua lista esta vazia")
-        else :
-            print("Esse livro não esta na lista")
+                break
+            elif livro == "sair" :
+                break
+        except FileNotFoundError:
+            print("Esse livro nao esta na lista")
+            break
         #Retornando a lista nova 
-    return lista 
+        f.close
+    return lista
