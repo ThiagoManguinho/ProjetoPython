b09321a09195f7d7e836979f25663d0616596156
DaviFernandoMatias <davi.fernando2005@gmail.com>
2023-11-22 20:42:00 -0300

    Finalizando printar biblioteca

--- projeto.py
+++ projeto.py
@@ -6,3 +6,17 @@ def lerArquivo(nome):
     except:
         #menssagem se tiver algum erro ao abrir o arquivo
         print('Erro ao ler o arquivo')
+    else:
+        #cabeçalho para a lista de livros
+        cabecalho('Livros Cadastrados')
+        print('Ordem informações: Livro - Autor - Categoria - Nível\n')
+        #separa os dados da linha do arquivo
+        for linha in a:
+            dado = linha.split(' - ')
+            #remove a quebra de linha
+            dado[1] = dado[1].replace('\n','')
+            #printa os Livros e suas características
+            print(f'{dado[0]}\t\t{dado[1]}\t\t{dado[2]}\t\t{dado[3]}')
+    finally:
+        #fecha o arquivo
+        a.close()
