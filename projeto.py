20c100e45cdb7012c8bd2c2bba739bfddc6e22a9
Bruno Mayer <brunobrmayer@gmail.com>
2023-11-22 21:01:53 -0300

    visualizar
    visualização dos livros

--- projeto.py
+++ projeto.py
@@ -1,45 +1,42 @@
-import os 
-os.system('cls')
+#editando as caracteristicas do livro
+        def editarLivro(nome, livro):
+            try:
+                #abre o arquivo em modo de leitura para obter os dados
+                with open(nome, 'r', encoding='utf-8') as arquivo:
+                    #lê todas as linhas do arquivo
+                    linhas = arquivo.readlines()
+                #variável para verificar se o livro foi encontrado
+                encontrado = False
+                #abre o arquivo em modo de escrita para realizar a edição
+                with open(nome, 'w', encoding='utf-8') as arquivo:
+                    #percorre todas as linhas do arquivo
+                    for linha in linhas:
+                        #divide a linha em dados separados pelo separador ' - '
+                        dados = linha.split(' - ')
+                        #verifica se o título do livro atual é igual ao título fornecido para edição
+                        if dados[0] == livro:
+                            #marca como encontrado
+                            encontrado = True
+                            #solicita novas informações para edição
+                            novo_autor = input(f'Digite o novo autor para o livro {livro}: ').title()
+                            nova_categoria = input(f'Digite a nova categoria para o livro {livro}: ').title()
+                            novo_nivel = input(f'Digite o novo nível para o livro {livro}: ').title()
+                            #atualiza a linha com as novas informações
+                            linha = f'{livro} - {novo_autor} - {nova_categoria} - {novo_nivel}\n'
+                            print(f'O livro "{livro}" foi editado com sucesso.')
+                        #escreve a linha no arquivo
+                        arquivo.write(linha)  
+                    
+                    # Se o livro não foi encontrado, exibe uma mensagem
+                    if not encontrado:
+                        print(f'O livro "{livro}" não foi encontrado na biblioteca.')
+            except FileNotFoundError:
+                print('Arquivo não encontrado.')
+            except Exception as e:
+                print(f'Ocorreu um erro: {str(e)}')
 
-livros = []  # Lista vazia para armazenar informações sobre os livros
-
-def visualizar_livros():
-    if not livros:
-        print('Nenhum livro cadastrado ainda.\n')
-        return
-
-    for livro in livros:
-        print(f'Nome: {livro["nome"]}, Autor: {livro["autor"]}, Categoria: {livro["categoria"]}, Custo: R${livro["custo"]:.2f}')
-    print()
-
-def atualizar_livro():
-    visualizar_livros()  # Chama a função para exibir os livros antes de atualizar
-
-    try:
-        nome_livro = input('Digite o nome do livro que deseja atualizar: ')  # Solicita o nome do livro que o usuário deseja atualizar
-        livro_encontrado = next((livro for livro in livros if livro["nome"] == nome_livro), None)  # Encontra o livro com o nome fornecido
-
-        if not livro_encontrado:
-            print(f'Livro com o nome "{nome_livro}" não encontrado.\n')  # Se o livro não for encontrado, exibe uma mensagem
-            return
-
-        # Exibe informações sobre o livro encontrado
-        print(f'Livro encontrado: Nome: {livro_encontrado["nome"]}, Autor: {livro_encontrado["autor"]}, Categoria: {livro_encontrado["categoria"]}, Custo: R${livro_encontrado["custo"]:.2f}')
-
-        # Solicita ao usuário novas informações sobre o livro
-        livro_encontrado["nome"] = input('Novo nome do livro (ou pressione Enter para manter o mesmo): ')
-        livro_encontrado["autor"] = input('Novo autor do livro (ou pressione Enter para manter o mesmo): ')
-        livro_encontrado["categoria"] = input('Nova categoria do livro (ou pressione Enter para manter a mesma): ')
-        novo_custo = input('Novo custo do livro (ou pressione Enter para manter o mesmo): ')
-
-        # Atualiza o custo apenas se o usuário fornecer um novo custo
-        if novo_custo:
-            livro_encontrado["custo"] = float(novo_custo)
-
-        print('Livro atualizado com sucesso!\n')  # Exibe uma mensagem de sucesso após a atualização
-
-    except Exception as e:
-        print(f'Ocorreu um erro: {e}\n')  # Lida com exceções e exibe uma mensagem de erro
-
-# Exemplo de uso
-atualizar_livro()  # Chama a função para atualizar um livro
+        #livro a ser editado
+        print(linha())
+        livro_editado = input('Digite o título do livro que deseja editar: ').title()
+        #chama a função para editar o livro na biblioteca
+        editarLivro(teste, livro_editado)
